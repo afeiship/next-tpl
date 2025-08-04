00030cc37abcdc0f9ca9b2e5e17cd765b8b3ecae
aric.zheng <1290657123@qq.com>
2025-08-04 16:26:44 +0800

    feat: add umd task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const { NxScripts, CleanRegistry } = require('@jswork/gulp-registry');
 
 const task1 = new CleanRegistry();
@@ -6,4 +9,16 @@ const task2 = new NxScripts({ name: 'qs', classify: false });
 
 [task1, task2].forEach(gulp.registry);
 
-gulp.task('default', gulp.series(['clean', 'nx:scripts']));
+
+gulp.task('umd', () => {
+  return gulp
+    .src('src/index.js') // 保存上面的 UMD 代码为 src/nx-tpl.js
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.umd' }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', gulp.series(['clean', 'nx:scripts', 'umd']));
